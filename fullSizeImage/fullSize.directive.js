b9ff0e39772d22b70e830b5c205002f2aa39036c
Alexey Potopakhin <alexey_potopahin@hotmail.com>
2015-07-17 18:10:23 +0300

    Improved fullSizeImage directive

--- fullSizeImage/fullSize.directive.js
+++ fullSizeImage/fullSize.directive.js
@@ -1,6 +1,6 @@
 (function() {
     angular
-        .module("photos")
+        .module("fullSizeImage", [])
         .directive("fullSizeImage", fullSizeImage);
 
     function fullSizeImage() {
@@ -23,13 +23,10 @@
                 // Background layer
                 var layer = document.createElement("div");
 
-                // Background layer styles
-                var layerStyle = document.createAttribute("style");
-                layerStyle.value = "position:fixed;" +
-                    "height:100%;width:100%;" +
-                    "background:rgba(0,0,0,0.6);" +
-                    "top:50%;left:50%;transform:translate(-50%,-50%);";
-                layer.setAttributeNode(layerStyle);
+                // Layer class
+                var layerClass = document.createAttribute("class");
+                layerClass.value = "full-size-image-container";
+                layer.setAttributeNode(layerClass);
 
                 // Full size image
                 var image = document.createElement("img");
@@ -38,12 +35,6 @@
                 var imageSrc = document.createAttribute("src");
                 imageSrc.value = imagePath;
                 image.setAttributeNode(imageSrc);
-
-                // Image styles
-                var imageStyle = document.createAttribute("style");
-                imageStyle.value = "position:fixed;" +
-                    "top:50%;left:50%;transform:translate(-50%,-50%);";
-                image.setAttributeNode(imageStyle);
 
                 layer.appendChild(image);
                 document.body.appendChild(layer);
